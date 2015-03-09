19c2c43dfcdcb1e8074ea3a8b73f9b944d0e62be
Kyle Nickel <nickelkr@gmail.com>
2015-03-09 18:49:35 -0400

    Added main page and templating

--- datalist.go
+++ datalist.go
@@ -4,10 +4,19 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"html/template"
 	"os"
 )
 
+type Entry struct {
+	Name 				string
+	Link 				string
+	Description string
+}
+
 var logger = setupLog()
+var templates = template.Must(template.ParseFiles("views/links.html"))
+var testEntries = []Entry{{"This", "one", "is a test"}}
 
 // SetupLog initializes our Logger and returns a pointer to the Logger
 func setupLog() *log.Logger {
@@ -28,6 +37,15 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+// Render executes the template for a given name
+func render(w http.ResponseWriter, tmpl string, entries []Entry) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", entries)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// Index returns the front-page of newly registered data sources
 func index(w http.ResponseWriter, r *http.Request) {
-  logger.Printf("In index")
+  render(w, "links", testEntries)
 }
